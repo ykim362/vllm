c1b078d6e2583657f22ee1a315bb541f361208b5
Young Jin Kim <youki@microsoft.com>
2024-06-17 10:09:56 -0700

    make it shorter

--- vllm/config.py
+++ vllm/config.py
@@ -1095,7 +1095,7 @@ def _get_and_verify_max_len(
         # that will be bigger than derived_max_model_len. We compare user input
         # with model_max_length and allow this override when it's smaller.
     #    model_max_length = getattr(hf_config, "model_max_length", None)
-        model_max_length = 16384
+        model_max_length = 12000
         if model_max_length is not None and max_model_len <= model_max_length:
             pass
         else:
